e7d0701c053942979d6e6236017c2b6e4d222988
lublubczyk <lubczyk@ukr.net>
2024-05-01 18:36:10 +0300

    empty project

---
+++ my-app/src/App.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import style from './App.module.css';
+
+function App() {
+  return (
+    <div className={style.app}>
+       Hello React
+    </div>
+  );
+}
+
+export { App };
